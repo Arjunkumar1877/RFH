18088644b468f42896695d2428f7547c2213b06f
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-10-06 21:49:35 +0530

    Added hook form devtool

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,26 +1,29 @@
 import { useForm } from "react-hook-form";
-
+import { DevTool } from '@hookform/devtools'
 export const YouTubeForm = () => {
     const form = useForm();
 
     console.log(form)
-    // const { register } = useForm();
+    const { register, control } = form;
+     console.log(register)
+
     return (
       <div>
         <h1>YouTube Form</h1>
   
         <form>
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" />
+          <input type="text" id="username"  {...register('username')} />
   
           <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" {...register('email')} />
   
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" name="channel" />
+          <input type="text" id="channel" {...register('channel')} />
   
           <button>Submit</button>
         </form>
+        <DevTool control={control} />
       </div>
     );
   };
